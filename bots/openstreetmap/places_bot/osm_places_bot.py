8368a00a2a338fb592e2a003a3e0f8f6be4ff45b
Artem Dudarev <dudarev@gmail.com>
2014-08-27 12:08:37 +0300

    save places to mongo - #6

---
+++ bots/openstreetmap/places_bot/osm_places_bot.py
@@ -0,0 +1,184 @@
+"""
+Parses all nodes and circular ways with names. Saves them in GeoJSON format.
+
+No relations are parsed for now. This may result in duplicates when some nodes
+and ways are part of the same relation.
+"""
+
+from datetime import datetime
+import os
+import plyvel
+from shutil import rmtree
+import subprocess
+
+import geojson
+from imposm.parser import OSMParser
+
+
+# EXTRACT_URL = "https://s3.amazonaws.com/metro-extracts.mapzen.com/berlin.osm.bz2"
+# EXTRACT_URL = "https://s3.amazonaws.com/metro-extracts.mapzen.com/odessa.osm.bz2"
+# EXTRACT_URL = "map.osm"
+EXTRACT_URL = "berdyansk.osm.bz2"
+FILENAME = EXTRACT_URL.split('/')[-1]
+
+
+if not os.path.exists(FILENAME):
+    print 'downloading file'
+    subprocess.call(['wget', EXTRACT_URL])
+else:
+    print 'file exists'
+
+
+class Coords():
+    def __init__(self, db):
+        self.db = db
+
+    def coord_precache(self, coords):
+        for id, lat, lon in coords:
+            self.db.put(str(id), str(lat) + ',' + str(lon))
+
+
+class Place(object):
+    """Place can be a node, a way or a relation.
+    """
+
+    def __init__(self, name, id=None, coordinates=None, tags=None, refs=None, members=None):
+        self.id = id
+        self.name = name
+        self.coordinates = coordinates
+        self.tags = tags
+        self.refs = refs  # used in ways
+        self.memebers = members  # used in relations
+        self.last_crawled = datetime.utcnow()
+
+    @staticmethod
+    def find_bbox_center(refs):
+        coords = []
+        for ref in refs:
+            coord = coordsDB.get(str(ref))
+            if coord:
+                coord = map(float, coord.split(','))
+                coords.append(coord)
+        if coords:
+            lons = [c[0] for c in coords]
+            lats = [c[1] for c in coords]
+            return (
+                (max(lons) + min(lons)) / 2.,
+                (max(lats) + min(lats)) / 2.)
+        else:
+            return None
+
+    def update_coordinates(self):
+        if not self.coordinates:
+            if self.refs:
+                self.coordinates = self.find_bbox_center(self.refs)
+            elif self.members:
+                self.coordinates = self.find_bounding_box(self.members)
+
+    def to_feature(self):
+        self.update_coordinates()
+        if self.coordinates:
+            point = geojson.Point(self.coordinates)
+            return geojson.Feature(
+                geometry=point,
+                id=self.id,
+                properties={"tags": self.tags, "name": self.name})
+        return None
+
+
+# simple class that handles parsed OSM data
+class PlaceCollector(object):
+    def __init__(self):
+        self.places = []
+        self.ways_count = 0
+        self.nodes_count = 0
+        self.relations_count = 0
+
+    def nodes(self, nodes):
+        for osmid, tags, coords in nodes:
+            name = tags.get('name', '')
+            osmid = 'osm/n/' + str(osmid)
+            if name:
+                self.nodes_count += 1
+                self.places.append(
+                    Place(name=name, id=osmid, coordinates=coords, tags=tags))
+                if self.nodes_count % 1000 == 0:
+                    print 'nodes: ', self.nodes_count
+
+    def ways(self, ways):
+        for osmid, tags, refs in ways:
+            name = tags.get('name', '')
+            # circular ways with name only
+            if name and len(tags) and refs[0] == refs[-1]:
+                osmid = 'osm/w/' + str(osmid)
+                self.ways_count += 1
+                self.places.append(
+                    Place(name=name, id=osmid, tags=tags, refs=refs))
+                if self.ways_count % 1000 == 0:
+                    print 'ways: ', self.ways_count
+
+    def relations(self, relations):
+        for osmid, tags, members in relations:
+            # no relations are parsed for now
+            if 'name' in tags:
+                self.relations_count += 1
+
+    @property
+    def __geo_interface__(self):
+        return geojson.FeatureCollection(
+            [p.to_feature() for p in self.places])
+
+    def update_coordinates(self):
+        for p in self.places:
+            if p.coordinates is None:
+                p.update_coordinates()
+
+    def show(self):
+        from pymongo import MongoClient
+        client = MongoClient()
+        db = client.locodb
+        for p in self.places:
+            print p.name
+            print p.coordinates
+            print p.tags
+            print p.id
+            db.places.update({'_id': p.id}, {'name': p.name}, upsert=True)
+            print '---'
+
+
+if __name__ == '__main__':
+
+    if os.path.isdir('coords.ldb'):
+        rmtree('coords.ldb')
+
+    coordsDB = plyvel.DB(
+        'coords.ldb',
+        create_if_missing=True,
+        error_if_exists=True,
+        write_buffer_size=1048576 * 1024)
+
+    coords = Coords(coordsDB)
+    p = OSMParser(coords_callback=coords.coord_precache)
+    print 'caching all coordinates'
+    p.parse(FILENAME)
+
+    # instantiate collector and parser and start parsing
+    collector = PlaceCollector()
+    p = OSMParser(
+        concurrency=4,
+        nodes_callback=collector.nodes,
+        ways_callback=collector.ways,
+        relations_callback=collector.relations)
+    p.parse(FILENAME)
+
+    # done
+    print 'Number of nodes, ways, relations corresponding to populated places'
+    print 'nodes: ', collector.nodes_count
+    print 'ways: ', collector.ways_count
+    print 'relations: ', collector.relations_count
+
+    # with open('poi.json', 'w') as f:
+    #     geojson.dump(collector, f)
+
+    collector.update_coordinates()
+    collector.show()
