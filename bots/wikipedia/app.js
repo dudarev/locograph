abb828bbea023b27035d8cb09e1c1d43b73c010e
Dmitriy Ugnichenko <mitya@mityaz.co.ua>
2014-09-02 23:57:40 +0300

    First steps in crawling cities information.

--- bots/wikipedia/app.js
+++ bots/wikipedia/app.js
@@ -31,14 +31,46 @@ var download = function(url, callback) {
   });
 }
 
-var parseLinks = function(data) {
-  console.dir(data);
-  return data;
+var prepareDBCollection = function() {
+  throw("Not yet implemented")
 }
 
-var store = function(data) {
+var storeData = function(data) {
   console.dir(data);
-  return data;
+}
+
+var parseLink = function(link) {
+  download(link, function(data) {
+    if(data) {
+      var result = {}
+        , $ = cheerio.load(data);
+      result["URL"] = link;
+      result["is_city"] = true;
+
+      // TODO Investigate support for all jQuery filters
+
+      // result["population"] = $('table.infobox.geography.vcard th:contains(Population)').parent().next().find('th:contains(Total)')//.next().text();
+      // var census_date = $('table.infobox.geography.vcard th:contains(Population)').
+                          // text().match(/(\d{4})-(\d{2})-(\d{2})/);
+      // result["census_date"] = new Date(census_date[1], census_date[2] - 1, census_date[3]);
+      // result["coords"] = { latitude : $('table.infobox.geography.vcard td:contains(Coordinates) span.latitude').first().text(), // bad selector, investigate
+                           // longtitude : $('table.infobox.geography.vcard td:contains(Coordinates) span.longitude').first().text()
+                         // };
+
+      result["last_crawled"] = new Date();
+      storeData(result);
+    } else {
+      console.dir("Could not get data for link: " + link);
+    }
+  });
+}
+
+
+var parseLinks = function(data) {
+  for(var i = 0; i < data.length; i++) {
+    parseLink(data[i]);
+  }
+  return;
 }
 
 // Get links from the cities url, using JQuery-like syntax
@@ -60,6 +92,9 @@ var get_links = function() {
 
 
 // main part
+
+// Opening connection to a database and creating necessary collections
+// prepareDBCollection();
 
 // Getting links using provided time interval
 if(argv.hasOwnProperty("t")) {
